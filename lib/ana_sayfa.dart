20af40e45c1e5f7512b4199bddf83e6afa91beaf
Samet Akışık <samet_ak539@hotmail.com>
2023-12-18 00:53:50 +0300

    ana sayfa tasarımı yapıldı. pageview eklendi

--- lib/ana_sayfa.dart
+++ lib/ana_sayfa.dart
@@ -73,20 +73,28 @@ class anaSayfa extends StatelessWidget {
         ],*/
         backgroundColor: Color.fromARGB(255, 117, 23, 239),
       ),
-      body: Stack(children: [
-        Container(
-          padding: EdgeInsets.all(20),
-          decoration: genelTema(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                  margin: EdgeInsets.only(bottom: 20),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        decoration: genelTema(),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                  margin: EdgeInsets.only(right: 100,  ),
+                  //padding: EdgeInsets.only(top: 3),
                   child: ZamanMetni(
                     name: widget.username,
                   )),
-              Container(
-                  margin: EdgeInsets.only(bottom: 20),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+
+                  margin: EdgeInsets.only(bottom: 0, top:10),
                   child: GestureDetector(
                     onTap: () {
                       Navigator.pushNamed(context, '/AramaSayfasi');
@@ -95,14 +103,6 @@ class anaSayfa extends StatelessWidget {
                       absorbing:
                           true, // AbsorbPointer'ı true olarak ayarlayarak dokunma etkisizleştirilir.
                       child: Container(
-                        decoration: BoxDecoration(boxShadow: [
-                          BoxShadow(
-                            color: renk2(),
-                            spreadRadius: 25, // Gölgeli alanın genişliği
-                            blurRadius: 10, // Gölgeli alanın bulanıklığı
-                            //offset: Offset(0),
-                          )
-                        ]),
                         child: TextField(
                           controller: _searchController,
                           decoration: const InputDecoration(
@@ -119,101 +119,137 @@ class anaSayfa extends StatelessWidget {
                       ),
                     ),
                   )),
-              SizedBox(
-                height: 150,
-                child: Stack(
-                  children: [
-                    ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: songs.length,
-                      itemBuilder: (context, index) {
-                        return SizedBox(
-                          width: 200,
-                          child: Stack(
-                            alignment: Alignment.bottomCenter,
-                            children: [
-                              ElevatedButton(
+            ),
 
-                                style: ElevatedButton.styleFrom(
-                                  padding: EdgeInsets.zero, // Padding'i kaldır
-                                  shape: const RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20)),
-                                  ),
-                                  backgroundColor: Colors
-                                      .transparent, // Düğme arka planını saydam yap
-                                  elevation: 0, // Gölgeyi kaldır
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(
-                                      20), // Resmi yuvarlat
-                                  child: Image.asset(
-                                    songs[index].coverUrl,
-                                    fit: BoxFit
-                                        .cover, // Resmi tamamen kaplayacak şekilde ayarla
-                                  ),
-                                ),
-                                onPressed: () {}, // Boş bir fonksiyon
-                              ),
-                              Positioned(
-                                child: Card(
-                                  child: GestureDetector(
-                                    onTap:(){Navigator.push(context,
-                                        MaterialPageRoute(builder: (context) =>
-                                        (TurCalmaListesi(turIndex: index ,))));
-                                      } ,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(left: 8.0),
-                                        child: Row(
-                                          children: [
-                                            Expanded(
-                                              flex: 7,
-                                              child: Center(
-                                                child: Text(
-                                                  songs[index].title,
-                                                  style: TextStyle(
-                                                      color: renk2(),
-                                                      fontSize: 25,
-                                                      fontWeight:
-                                                          FontWeight.w900),
-                                                ),
-                                              ),
-                                            ),
-                                            Expanded(
-                                                flex: 3,
-                                                child: Icon(
-                                                  Icons.play_circle,
-                                                  color: renk2(),
-                                                  size: 33,
-                                                )),
-                                          ],
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceAround,
+            Expanded(
+              flex: 6,
+              child: Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [SizedBox(
+                      height: 34,
+                      child: Text("  Trend müzikler", style: TextStyle(fontSize: 20, color:beyaz(), fontWeight: FontWeight.bold,), )),
+                    SizedBox(
+                      height: 150,
+                      child: Stack(
+                        children: [
+                          ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: songs.length,
+                            itemBuilder: (context, index) {
+                              return SizedBox(
+                                width: 200,
+                                child: Stack(
+                                  alignment: Alignment.bottomCenter,
+                                  children: [
+                                    ElevatedButton(
+
+                                      style: ElevatedButton.styleFrom(
+                                        padding: EdgeInsets.zero, // Padding'i kaldır
+                                        shape: const RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.all(Radius.circular(20)),
+                                        ),
+                                        backgroundColor: Colors
+                                            .transparent, // Düğme arka planını saydam yap
+                                        elevation: 0, // Gölgeyi kaldır
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(
+                                            20), // Resmi yuvarlat
+                                        child: Image.asset(
+                                          songs[index].coverUrl,
+                                          fit: BoxFit
+                                              .cover, // Resmi tamamen kaplayacak şekilde ayarla
                                         ),
                                       ),
+                                      onPressed: () {}, // Boş bir fonksiyon
                                     ),
-                                  ),
-                                  color: Colors.white70.withOpacity(0.8),
+                                    Positioned(
+                                      child: Card(
+                                        child: GestureDetector(
+                                          onTap:(){Navigator.push(context,
+                                              MaterialPageRoute(builder: (context) =>
+                                              (TurCalmaListesi(turIndex: index ,))));
+                                            } ,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(4.0),
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(left: 8.0),
+                                              child: Row(
+                                                children: [
+                                                  Expanded(
+                                                    flex: 7,
+                                                    child: Center(
+                                                      child: Text(
+                                                        songs[index].title,
+                                                        style: TextStyle(
+                                                            color: renk2(),
+                                                            fontSize: 25,
+                                                            fontWeight:
+                                                                FontWeight.w900),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Expanded(
+                                                      flex: 3,
+                                                      child: Icon(
+                                                        Icons.play_circle,
+                                                        color: renk2(),
+                                                        size: 33,
+                                                      )),
+                                                ],
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.spaceAround,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        color: Colors.white70.withOpacity(0.8),
+                                      ),
+                                      width: 150,
+                                      height: 50,
+                                      bottom: 10,
+                                      left: 25,
+                                    ),
+                                  ],
                                 ),
-                                width: 150,
-                                height: 50,
-                                bottom: 10,
-                                left: 25,
-                              ),
-                            ],
+                              );
+                            },
                           ),
-                        );
-                      },
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              flex: 9,
+              child: Container(
+                margin: EdgeInsets.only(top: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: Text("  Çalma listelerim", style: TextStyle(fontSize: 20, color: beyaz(), fontWeight: FontWeight.w700),  )),
+                    Playlists("playlist 1"),
+                    Playlists("playlist 2"),
+                    Playlists("playlist 3"),
+
+
+
+                  ],
+                ),
+              ),
+            ),
+
+          ],
         ),
-      ]),
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Color.fromARGB(255, 117, 23, 239),
         child: Row(
@@ -255,6 +291,23 @@ class anaSayfa extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Card Playlists(title) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 20, ),
+      shadowColor: Colors.black,
+      elevation: 10,
+      color: renk2(),
+                  child: ListTile(
+                    title: Text(title, style: TextStyle(color: beyaz(), fontWeight: FontWeight.bold, fontSize: 17),),
+                    subtitle: Text("3 şarkı", style: TextStyle(color: beyaz(), ),),
+                    leading: Image.network("https://picsum.photos/200/300"),
+                    trailing: IconButton(icon:Icon(Icons.play_circle, color: beyaz()),
+
+                      onPressed: (){},  ),
+                  ),
+                );
   }
 }
 
@@ -325,8 +378,8 @@ class ZamanMetni extends StatelessWidget {
     return Text(
       getGreeting(),
       style: TextStyle(
-        fontSize: 25,
-        fontWeight: FontWeight.bold,
+        fontSize: 30,
+        fontWeight: FontWeight.w900,
         color: Colors.white,
       ),
       textAlign: TextAlign.left,
